a97204d465ad219e1be721651d3cc72f8ae0397d
INGANZAMARUMPU <jonkur96@gmail.com>
2022-11-25 19:52:41 +0200

    ionClick to say hello world

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import {
   IonApp, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonicVue,
-  IonRouterOutlet 
+  IonRouterOutlet, IonCol, IonButton, IonItem, IonInput, IonLabel
 } from '@ionic/vue';
 import '@ionic/core/css/ionic.bundle.css'
 
@@ -17,5 +17,10 @@ app.component("IonHeader", IonHeader)
 app.component("IonToolbar", IonToolbar)
 app.component("IonTitle", IonTitle)
 app.component("IonContent", IonContent)
+app.component("IonCol", IonCol)
+app.component("IonButton", IonButton)
+app.component("IonItem", IonItem)
+app.component("IonInput", IonInput)
+app.component("IonLabel", IonLabel)
 
 app.mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,44 @@
         <ion-header>
             <ion-toolbar>
                 <ion-title>URUFATANAMENDE</ion-title>
+
             </ion-toolbar>
         </ion-header>
         <ion-content>
             <ion-item>
                 <ion-label position="floating">Witwa nde?</ion-label>
-                <ion-input :clear-input="true" placeholder="Votre nom SVP"></ion-input>
+                <ion-input
+                  :clear-input="true"
+                  placeholder="Votre nom SVP"
+                  @ionChange="e => nom = e.target.value"
+                  />
             </ion-item>
+            <br>
+            <ion-button @click="saluer">
+              Saluer
+            </ion-button>
+            <br>
+            <br>
+            <ion-col>
+              {{ salutation }}
+            </ion-col>
         </ion-content>
     </ion-page>
 </template>
 <script>
-export default {}
+export default {
+  data(){
+    return {
+      salutation:'',
+      nom:''
+    }
+  },
+  methods:{
+    saluer(){
+      this.salutation = `BONJOUR ${this.nom}`
+    }
+  }
+}
 </script>
 <style scoped>
 </style>
